25d831993ef08eb872ee9ab2074546cdc432bb6b
tianzhi <1095098525@qq.com>
2020-04-02 21:40:36 +0800

    source

--- src/react-mini/sreact-dom.js
+++ src/react-mini/sreact-dom.js
@@ -2,6 +2,7 @@ import initVdom from './svdom'
 
 function render(vdom, container) {
     // container.innerHTML = `<pre>${JSON.stringify(vdom, null, 2)}</pre>`;
+    
     const node = initVdom(vdom)
     container.appendChild(node)
 }
